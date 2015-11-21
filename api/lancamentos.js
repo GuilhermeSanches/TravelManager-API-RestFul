04154846f5ade6d97a8b38d0782e4bc4b04a8bca
Guilherme Sanches Pereira <guilherme_1993pa@hotmail.com>
2015-11-21 21:29:17 -0200

    add create viagem

--- api/lancamentos.js
+++ api/lancamentos.js
@@ -2,7 +2,7 @@ exports.read = function(req, res) {
 var id = req.params.id;
     
 	req.getConnection(function(err,connection){
-		connection.query('SELECT * FROM despesas l WHERE l.id_viagem = ?',[id],function(err,result){
+		connection.query('SELECT l.id, l.valor, l.id_viagem, c.nome, l.data FROM despesas l join categoria c on c.id = l.id_categoria WHERE l.id_viagem = ?',[id],function(err,result){
 			if(err) return res.status(400).json();
 			return res.status(200).json(result);
 		});
