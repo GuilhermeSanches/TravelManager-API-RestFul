8dc4d02308bfccb0f2257a638de53f7dd3d9a855
Guilherme Sanches Pereira <guilherme_1993pa@hotmail.com>
2015-11-22 16:44:11 -0200

    correção arquivos excluidos desnecessários

--- app/js/api/googleMaps.js
+++
@@ -1,7 +0,0 @@
-var map;
-function initMap(latitude, longitude) {  
-  map = new google.maps.Map(document.getElementById("google"), {
-    center: {lat: parseFloat(latitude), lng: parseFloat(longitude)},
-    zoom: 16
-  });
-};
