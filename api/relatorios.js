beb39d7963daad86b4ffc95566d03ca700034bb3
JessicaAdriele <jessica-adriele2011@live.com>
2015-11-22 23:24:47 -0200

    add implementacao relatorio

--- api/relatorios.js
+++ api/relatorios.js
@@ -3,9 +3,11 @@ exports.read = function(req, res) {
 	var idViagem = req.params.id;
     
 	req.getConnection(function(err,connection){
-		connection.query('SELECT c.nome, coalesce(sum(d.valor), 0)as valor FROM `categoria` c left join despesas d on c.id = d.id_categoria WHERE d.id_viagem = ? or d.id_categoria is null group by 1 order by 1',[idViagem],function(err,result){
+		connection.query('select nome, coalesce(t1.valor, 0)as valor from categoria c left join(select coalesce(sum(d.valor), 0)as valor, d.id_categoria from despesas d where d.id_viagem = 48)as t1 on c.id = t1.id_categoria order by 1',[idViagem],function(err,result){
 			if(err) return res.status(400).json();
 			return res.status(200).json(result);
 		});
 	});
 }
+
+
